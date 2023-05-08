cc0be55ee92b79977a63d34733ae9f5c6747a125
Nardics <bmo230@uky.edu>
2023-05-08 09:02:40 -0500

    Update config.js

--- config.js
+++ config.js
@@ -421,7 +421,7 @@ var config = {
 
       <p>A cursory glance at the map reveals clear lines of demarcation between concentrations of Persons of Color, and other ethnicities.
       A strong correlation also exists between the median incomes and the distribution of Persons of Color. As highlighted
-      in the <a href='https://doee.dc.gov/sites/default/files/dc/sites/ddoe/service_content/attachments/Methodology%20Report_Update%2005.11.22web_0.pdf'>CADMUS Report <a/> ,
+      in the <a href='https://doee.dc.gov/sites/default/files/dc/sites/ddoe/service_content/attachments/Methodology%20Report_Update%2005.11.22web_0.pdf'>CADMUS Report</a>,
       historical and structural patterns can influence health risks and outcomes.</p> 
 
       <p>Census Tracts with the highest median income, are sparsely populated by Persons of Color. This naturally also holds true for 
